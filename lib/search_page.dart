862485682e2db24b92200d762854c98fe5aee8d5
SID-CJ <sidharthcj2004@gmail.com>
2024-04-03 15:28:13 +0530

    community

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,15 +1,65 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
+
+  void _search() {
+    setState(() {
+      _searchQuery = _searchController.text;
+      // You can implement your search functionality here
+      // For now, let's just print the search query
+      print('Searching for: $_searchQuery');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search'),
+        title: Text('Community'),
       ),
-      body: Center(
-        child: Text('This is the Search Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Find a user',
+                  labelText: 'Enter Username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0), // Rounded edges
+                  ),
+                ),
+                onChanged: (value) {
+                  // You can perform live search as the user types if needed
+                },
+              ),
+            ),
+            SizedBox(width: 10), // Add space between search bar and icon button
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: _search,
+              iconSize: 30,
+              color: Theme.of(context)
+                  .primaryColor, // Use the primary color of your theme
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: SearchPage(),
+  ));
 }
